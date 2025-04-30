b3661136e25780198c74a16e09d255abc2169eef
sparkychild <sparkychildcharlie@gmail.com>
2025-04-29 22:53:36 -0400

    Add more pages

---
+++ data/performances.js
@@ -0,0 +1,11 @@
+const Upcoming = [
+    "Solo recital- 7:00pm, April 23rd. Church of the Ascension, 253 Echo Dr., Ottawa"
+]
+
+const Past = [
+    "MMus graduating recital -8:00pm, March 16th, 2023. Von Kuster Hall, Western University.",
+    "1st year MMus recital -8:00pm, March 11th, 2022. Von Kuster Hall, Western University.",
+    "Graduating Recital - 4:00pm, April 18th, 2018. Tabaret Hall, University of Ottawa.",
+    "Duet Recital with Echo Lau - 3:00pm, September 17th, 2017. Freiman Hall, University of Ottawa.",
+    "Recital - 4:00pm, March 19th, 2017. Freiman Hall, University of Ottawa."
+]
--- js/script.js
+++ js/script.js
@@ -6,20 +6,29 @@ function buildMenu(image) {
     const items = ['HOME', 'REPERTOIRE', 'BIO', 'MEDIA', 'PERFORMANCES', 'CONTACT']
     const buttons = items.map(name => {
         let id = toId(name);
-        if (id === 'home') id = '';
+        if (id === 'home') id = 'index';
 
-        return `<a class="menu-button" href="${id}">${name}</a>`;
+        return `<a class="menu-button" href="${id}.html">${name}</a>`;
     })
     const spacing = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
     
-    let menu = document.getElementById('menu');
+    const menu = document.getElementById('menu');
 
-    if (image) menu.backgroundImage = `images/${image}`;
-    else menu.height = '100px';
+    if (image) {
+        menu.style.backgroundImage = `url("images/${image}")`;
+        menu.style.color = 'white';
+    } else {
+        menu.style.height = '50px';
+    }
 
     menu.innerHTML = buttons.join(spacing);
 }
 
-function fill() {
+function loadPerformances() {
+    let elem = document.getElementById('contents');
 
+    let buffer = '';
+    buffer += `Upcoming:<br /><ul style="padding-left: 50px">${Upcoming.map(l => `<li>${l}</li>`).join('')}</ul><br />`
+    buffer += `Past:<br /><ul style="padding-left: 50px">${Past.map(l => `<li>${l}</li>`).join('')}</ul>`;
+    elem.innerHTML = buffer;
 }
